e5ceefc444033309aec3887eb63eaa5e38360bcb
Eoin <eoin@kicker-matchday.com>
2023-04-11 16:58:04 +0200

    Add simple debug controller to verify entities are persisted

---
+++ src/main/java/com/homework/comyno/nlf/controllers/DebugController.java
@@ -0,0 +1,29 @@
+package com.homework.comyno.nlf.controllers;
+
+import com.homework.comyno.nlf.entities.Book;
+import com.homework.comyno.nlf.entities.Student;
+import com.homework.comyno.nlf.repositories.BookRepository;
+import com.homework.comyno.nlf.repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DebugController {
+
+  @Autowired
+  BookRepository bookRepository;
+
+  @Autowired
+  StudentRepository studentRepository;
+
+  @GetMapping("/books")
+  public Iterable<Book> getBooks() {
+    return bookRepository.findAll();
+  }
+
+  @GetMapping("/students")
+  public Iterable<Student> getStudents() {
+    return studentRepository.findAll();
+  }
+}
