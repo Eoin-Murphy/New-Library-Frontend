4a5a875212c8741720f15f97f26c81e2acc825e7
Eoin <eoin@kicker-matchday.com>
2023-04-11 16:06:54 +0200

    Barebones Spring Boot App

---
+++ src/main/java/com/homework/comyno/nlf/NlfApplication.java
@@ -0,0 +1,13 @@
+package com.homework.comyno.nlf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homework/comyno/nlf/NlfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.comyno.nlf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
